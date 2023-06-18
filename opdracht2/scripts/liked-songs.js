35c3beb39a4625797457e2550cfde089fefb5331
senanurkorkutan <snnrkrk1@gmail.com>
2023-06-18 18:29:56 +0200

    :test_tube: some additional code

--- opdracht2/scripts/liked-songs.js
+++ opdracht2/scripts/liked-songs.js
@@ -1,20 +1,40 @@
-// LOCAL STORAGE
-likedSongs = JSON.parse(localStorage.getItem('likedHearts')) || [];
+// // LOCAL STORAGE
+// likedSongs = JSON.parse(localStorage.getItem("likedHearts")) || [];
 
-// Create HTML elements for each liked song
-const likedSongsList = document.getElementById('likedSongsList');
+// // Create HTML elements for each liked song
+// const likedSongsList = document.getElementById("likedSongsList");
 
-likedSongs.forEach(song => {
-  const listItem = document.createElement('li');
-  const image = document.createElement('img');
-  image.src = song.imageSrc; // Make sure the image source URL is set correctly
-  const title = document.createElement('h2');
-  title.textContent = song.title;
-  const artist = document.createElement('h3');
-  artist.textContent = song.artist;
+// likedSongs.forEach((song) => {
+//     const listItem = document.createElement("li");
+//     const image = document.createElement("img");
+//     image.src = song.imageSrc;
+//     const title = document.createElement("h2");
+//     title.textContent = song.title;
+//     const artist = document.createElement("h3");
+//     artist.textContent = song.artist;
 
-  listItem.appendChild(image);
-  listItem.appendChild(title);
-  listItem.appendChild(artist);
-  likedSongsList.appendChild(listItem);
+//     listItem.appendChild(image);
+//     listItem.appendChild(title);
+//     listItem.appendChild(artist);
+//     likedSongsList.appendChild(listItem);
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const likedSongsList = document.getElementById("likedSongsList");
+    const likedSongs = JSON.parse(localStorage.getItem("likedHearts")) || [];
+
+    likedSongs.forEach((song) => {
+        const listItem = document.createElement("li");
+        const image = document.createElement("img");
+        image.src = song.imageSrc;
+        const title = document.createElement("h2");
+        title.textContent = song.title;
+        const artist = document.createElement("h3");
+        artist.textContent = song.artist;
+
+        listItem.appendChild(image);
+        listItem.appendChild(title);
+        listItem.appendChild(artist);
+        likedSongsList.appendChild(listItem);
+    });
 });
