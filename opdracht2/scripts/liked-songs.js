58abeb70e54c08a2ec32292febbc7ebed78637aa
senanurkorkutan <snnrkrk1@gmail.com>
2023-06-19 02:46:39 +0200

    :clown_face: DONE

--- opdracht2/scripts/liked-songs.js
+++ opdracht2/scripts/liked-songs.js
@@ -1,24 +1,4 @@
-// // LOCAL STORAGE
-// likedSongs = JSON.parse(localStorage.getItem("likedHearts")) || [];
-
-// // Create HTML elements for each liked song
-// const likedSongsList = document.getElementById("likedSongsList");
-
-// likedSongs.forEach((song) => {
-//     const listItem = document.createElement("li");
-//     const image = document.createElement("img");
-//     image.src = song.imageSrc;
-//     const title = document.createElement("h2");
-//     title.textContent = song.title;
-//     const artist = document.createElement("h3");
-//     artist.textContent = song.artist;
-
-//     listItem.appendChild(image);
-//     listItem.appendChild(title);
-//     listItem.appendChild(artist);
-//     likedSongsList.appendChild(listItem);
-// });
-
+// // // LOCAL STORAGE
 document.addEventListener("DOMContentLoaded", function () {
     const likedSongsList = document.getElementById("likedSongsList");
     const likedSongs = JSON.parse(localStorage.getItem("likedHearts")) || [];
@@ -37,4 +17,111 @@ document.addEventListener("DOMContentLoaded", function () {
         listItem.appendChild(artist);
         likedSongsList.appendChild(listItem);
     });
+
+    // DRAG AND DROP
+    Sortable.create(likedSongsList, {
+        swapThreshold: 0.5,
+        ghostClass: "sortableSong",
+        dragClass: "sortableDrag",
+        onMove: function (event) {
+            setTimeout(function () {
+                document.querySelectorAll(".sortableSong, li:nth-of-type(-n+2), li:nth-last-of-type(-n+2)").forEach(function (likedSongs) {
+                    checkIfFirst(likedSongs);
+                    checkIfLast(likedSongs);
+                });
+            }, 10);
+        },
+        onEnd: function (event) {
+            event.item.focus();
+        },
+    });
+
+    // SORT
+    function sortSongs(option) {
+        const songItems = Array.from(likedSongsList.getElementsByTagName("li"));
+        songItems.sort((a, b) => {
+            const textA = a.querySelector("h2").textContent.toLowerCase();
+            const textB = b.querySelector("h2").textContent.toLowerCase();
+
+            if (option === "az") {
+                return textA.localeCompare(textB);
+            } else if (option === "za") {
+                return textB.localeCompare(textA);
+            } else if (option === "short") {
+                return textA.length - textB.length;
+            } else if (option === "long") {
+                return textB.length - textA.length;
+            }
+        });
+
+        songItems.forEach((item) => likedSongsList.appendChild(item));
+    }
+
+    function sortArtists(option) {
+        const songItems = Array.from(likedSongsList.getElementsByTagName("li"));
+        songItems.sort((a, b) => {
+            const textA = a.querySelector("h3").textContent.toLowerCase();
+            const textB = b.querySelector("h3").textContent.toLowerCase();
+
+            if (option === "artistAz") {
+                return textA.localeCompare(textB);
+            } else if (option === "artistZa") {
+                return textB.localeCompare(textA);
+            } else if (option === "artistShort") {
+                return textA.length - textB.length;
+            } else if (option === "artistLong") {
+                return textB.length - textA.length;
+            }
+        });
+
+        songItems.forEach((item) => likedSongsList.appendChild(item));
+    }
+
+    document.getElementById("sortAZ").addEventListener("click", function () {
+        sortSongs("az");
+    });
+    document.getElementById("sortZA").addEventListener("click", function () {
+        sortSongs("za");
+    });
+    document.getElementById("sortShort").addEventListener("click", function () {
+        sortSongs("short");
+    });
+    document.getElementById("sortLong").addEventListener("click", function () {
+        sortSongs("long");
+    });
+
+    document.getElementById("AZ").addEventListener("click", function () {
+        sortArtists("artistAz");
+    });
+    document.getElementById("ZA").addEventListener("click", function () {
+        sortArtists("artistZa");
+    });
+    document.getElementById("Short").addEventListener("click", function () {
+        sortArtists("artistShort");
+    });
+    document.getElementById("Long").addEventListener("click", function () {
+        sortArtists("artistLong");
+    });
 });
+
+// SEARCH
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+
+searchButton.addEventListener("click", performSearch);
+
+function performSearch() {
+    const query = searchInput.value.trim().toLowerCase();
+    const songItems = document.querySelectorAll("#likedSongsList li");
+
+    songItems.forEach((songItem) => {
+        const songName = songItem.querySelector("h2").textContent.toLowerCase();
+        const artistName = songItem.querySelector("h3").textContent.toLowerCase();
+
+        if (songName.includes(query) || artistName.includes(query)) {
+            songItem.style.display = "grid";
+        } else {
+            songItem.style.display = "none";
+        }
+    });
+}
--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,71 +1,114 @@
-
 // HEART FEEDBACK FOR USER
-document.addEventListener('DOMContentLoaded', function() {
-  const likeHearts = document.querySelectorAll('.like');
-  
-  likeHearts.forEach(function(heart) {
-    heart.addEventListener('click', function() {
-      heart.classList.toggle('liked');
-      
-      if (heart.classList.contains('liked')) {
-        heart.setAttribute('src', 'images/heart-filled.png');
-        heart.setAttribute('alt', 'heart-filled');
-      } else {
-        heart.setAttribute('src', 'images/heart.png');
-        heart.setAttribute('alt', 'heart');
-      }
-      
-      const songTitle = heart.parentNode.querySelector('h2').innerText;
-      const artistName = heart.parentNode.querySelector('h3').innerText;
-      
-      let likedSongs = localStorage.getItem('likedSongs');
-      if (!likedSongs) {
-        likedSongs = [songTitle];
-      } else {
-        likedSongs = JSON.parse(likedSongs);
-        likedSongs.push(songTitle);
-      }
-      
-      localStorage.setItem('likedSongs', JSON.stringify(likedSongs));
+document.addEventListener("DOMContentLoaded", function () {
+    const likeHearts = document.querySelectorAll(".like");
+
+    likeHearts.forEach(function (heart) {
+        heart.addEventListener("click", function () {
+            heart.classList.toggle("liked");
+
+            if (heart.classList.contains("liked")) {
+                heart.setAttribute("src", "images/heart-filled.png");
+                heart.setAttribute("alt", "heart-filled");
+            } else {
+                heart.setAttribute("src", "images/heart.png");
+                heart.setAttribute("alt", "heart");
+            }
+
+            const songTitle = heart.parentNode.querySelector("h2").innerText;
+            const artistName = heart.parentNode.querySelector("h3").innerText;
+
+            let likedSongs = localStorage.getItem("likedSongs");
+            if (!likedSongs) {
+                likedSongs = [songTitle];
+            } else {
+                likedSongs = JSON.parse(likedSongs);
+                likedSongs.push(songTitle);
+            }
+
+            localStorage.setItem("likedSongs", JSON.stringify(likedSongs));
+        });
     });
-  });
 });
-
-
 
 // LOCAL STORAGE
 let likedSongs = [];
 
 function handleLike(event) {
-  const likeButton = event.target;
+    const likeButton = event.target;
 
-  // Check if the parent LI element exists
-  const listItem = likeButton.closest('li');
-  if (!listItem) {
-    return; // Exit the function if the parent LI element doesn't exist
-  }
+    const listItem = likeButton.closest("li");
+    if (!listItem) {
+        return;
+    }
 
-  // Retrieve the song information
-  const imageSrc = listItem.querySelector('img').src;
-  const title = listItem.querySelector('h2').textContent;
-  const artist = listItem.querySelector('h3').textContent;
+    const imageSrc = listItem.querySelector("img").src;
+    const title = listItem.querySelector("h2").textContent;
+    const artist = listItem.querySelector("h3").textContent;
 
-  // Create an object with the song information
-  const song = {
-    imageSrc: imageSrc,
-    title: title,
-    artist: artist
-  };
+    const song = {
+        imageSrc: imageSrc,
+        title: title,
+        artist: artist,
+    };
 
-  // Add the liked song to the array in local storage
-  likedSongs.push(song);
-  localStorage.setItem('likedHearts', JSON.stringify(likedSongs));
+    likedSongs.push(song);
+    localStorage.setItem("likedHearts", JSON.stringify(likedSongs));
 }
 
-// Add event listeners to all like buttons
-const likeButtons = document.querySelectorAll('.like');
-likeButtons.forEach(button => {
-  button.addEventListener('click', handleLike);
+const likeButtons = document.querySelectorAll(".like");
+likeButtons.forEach((button) => {
+    button.addEventListener("click", handleLike);
 });
 
+// KEYBOARD
+// scroll -> down-up
+const mainElement = document.querySelector("main");
 
+mainElement.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowUp") {
+        // up
+        mainElement.scrollTop -= 50;
+        event.preventDefault();
+    } else if (event.key === "ArrowDown") {
+        // down
+        mainElement.scrollTop += 50;
+        event.preventDefault();
+    }
+});
+
+// scroll -> right-left
+const sortButtons = document.querySelectorAll("#sortingOptions button");
+let activeButtonIndex = 0;
+
+sortButtons[activeButtonIndex].focus();
+
+function sortSongs(sortType) {
+    // Implement your sorting logic here
+    console.log("Sorting songs:", sortType);
+}
+
+function handleButtonNavigation(event) {
+    const key = event.key;
+
+    if (key === "ArrowRight" && activeButtonIndex < sortButtons.length - 1) {
+        // right
+        activeButtonIndex++;
+        sortButtons[activeButtonIndex].focus();
+    } else if (key === "ArrowLeft" && activeButtonIndex > 0) {
+        // left
+        activeButtonIndex--;
+        sortButtons[activeButtonIndex].focus();
+    } else if (key === "Enter") {
+        sortButtons[activeButtonIndex].click();
+    }
+}
+
+document.addEventListener("keydown", handleButtonNavigation);
+
+// LOADING
+setTimeout(function () {
+    const loadingSpinner = document.querySelector(".loading-spinner");
+    if (loadingSpinner) {
+        loadingSpinner.style.display = "none";
+    }
+}, 4000);
